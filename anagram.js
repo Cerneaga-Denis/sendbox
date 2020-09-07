69e7759bc93fcce590437a206c96615a82ea4e56
Cerneaga Denis <cerneaga.denis@gmail.com>
2020-09-08 02:33:24 +0300

    temporary

---
+++ anagram.js
@@ -0,0 +1,70 @@
+function aclean(arr) {
+  let map = new Map();
+
+  for (let word of arr) {
+    // разбиваем слово на буквы, сортируем и объединяем снова в строку
+    let sorted = word.toLowerCase().split("").sort().join(""); // (*)
+    map.set(sorted, word);
+  }
+
+  return Array.from(map.values());
+}
+
+let arr = [
+  "nap",
+  "teachers",
+  "cheaters",
+  "PAN",
+  "ear",
+  "era",
+  "hectares",
+  "nap",
+  "teachers",
+  "cheaters",
+  "PAN",
+  "ear",
+  "era",
+  "hectares",
+  "nap",
+  "teachers",
+  "cheaters",
+  "PAN",
+  "ear",
+  "era",
+  "hectares",
+  "nap",
+  "teachers",
+  "cheaters",
+  "PAN",
+  "ear",
+  "era",
+  "hectares",
+  "nap",
+  "teachers",
+  "cheaters",
+  "PAN",
+  "ear",
+  "era",
+  "hectares",
+];
+
+function fnSpeedTest(fn, repeat, ...args) {
+  if (typeof fn === "object")
+    return "Функции getFnTime необходим аргумент в виде функции. ";
+
+  let start = Date.now();
+
+  while (repeat > 0) {
+    fn(...args);
+    repeat--;
+  }
+
+  let end = Date.now();
+
+  console.log(fn(...args) === undefined ? "" : fn(...args));
+  console.log("*****************************************");
+  return `Функция отработала за ${(end - start) / 1000} секунд, (${
+    end - start
+  } м/сек)`;
+}
+console.log(fnSpeedTest(aclean, 1000000, arr));
